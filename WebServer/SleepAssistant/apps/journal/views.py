145f39ff7f5d9bb16d60c08388e1856a49d4737b
Hongxia <hongxia.zhong@gmail.com>
2013-05-20 16:32:22 -0700

    added phone_base.html

--- WebServer/SleepAssistant/apps/journal/views.py
+++ WebServer/SleepAssistant/apps/journal/views.py
@@ -1,1 +1,5 @@
-# Create your views here.
+# django imports
+from django.template import RequestContext, Context, loader
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, render_to_response
+from django.contrib.auth.decorators import login_required
--- WebServer/SleepAssistant/urls.py
+++ WebServer/SleepAssistant/urls.py
@@ -1,13 +1,12 @@
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # url(r'^$', 'SleepAssistant.views.home', name='home'),
     # url(r'^SleepAssistant/', include('SleepAssistant.foo.urls')),
-
+    url(r'^sleep/$', 'SleepAssistant.apps.journal.views.sleep'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
