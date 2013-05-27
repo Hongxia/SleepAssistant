432922a0a021c987dcc7dfb1c52943195a150494
Hongxia <hongxia.zhong@gmail.com>
2013-05-27 01:06:26 -0700

    corrected database settings

--- WebServer/SleepAssistant/settings.py
+++ WebServer/SleepAssistant/settings.py
@@ -25,16 +25,14 @@ import dj_database_url
 DATABASES = {
     'default': dj_database_url.config(default='postgres://sleepdev:sleepdev@localhost:5432/sleepdb')
     
-    '''
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'sleepdb',             # Or path to database file if using sqlite3.
-        'USER': 'sleepdev',             # Not used with sqlite3.
-        'PASSWORD': 'sleepdev',         # Not used with sqlite3.
-        'DATABASE_HOST': '',             # Set to empty string for localhost. Not used with sqlite3.
-        'DATABASE_PORT': '' 
-    }
-    '''
+    #'default': {
+    #    'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+    #    'NAME': 'sleepdb',             # Or path to database file if using sqlite3.
+    #    'USER': 'sleepdev',             # Not used with sqlite3.
+    #    'PASSWORD': 'sleepdev',         # Not used with sqlite3.
+    #    'DATABASE_HOST': '',             # Set to empty string for localhost. Not used with sqlite3.
+    #    'DATABASE_PORT': '' 
+    #}
 }
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
